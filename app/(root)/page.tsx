081476f9ef5c21420289d3c6fda6c897e9a3c413
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-02-08 16:23:42 +0330

    Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,15 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const HomePage = async () => {
-  return <>Prostore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newset Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
