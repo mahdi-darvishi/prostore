0fd0cb297e2279eb0a8ac9ff50efabcb282f9737
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-02-09 14:32:25 +0330

    Sign In & Sign Out Action

--- lib/validators.ts
+++ lib/validators.ts
@@ -22,3 +22,10 @@ export const insertProductSchema = z.object({
   banner: z.string().nullable(),
   price: currenzy,
 });
+
+// Schema for sighin users in
+
+export const signInFormSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be least 6 chracters"),
+});
