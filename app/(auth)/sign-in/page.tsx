7c1856758574c41265de636e798caaf0f263e90b
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-02-09 14:40:49 +0330

    Auth Layout & Sign In Page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,42 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metaData: Metadata = {
+  title: "Sign IN",
+};
+
+const SighInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              width={100}
+              height={100}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign in</CardTitle>
+          <CardDescription className="text-center ">
+            Sign in to your account
+          </CardDescription>
+          <CardContent className="space-y-4">{/* FROM HERE */}</CardContent>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+};
+
+export default SighInPage;
