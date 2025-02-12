7cf591efe510df5e4f1735080a566111732ac34f
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-02-12 18:50:46 +0330

    Payment Method Form & Update

--- app/(root)/payment-method/payment-method-form.tsx
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -1,13 +1,24 @@
 "use client";
-import { useRouter } from "next/router";
 import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { paymentMethodSchema } from "@/lib/validators";
-import CheckoutSteps from "@/components/shared/checkout-steps";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { DEFAULT_PAYMENT_METHOD } from "@/lib/constants";
+import { DEFAULT_PAYMENT_METHOD, PAYMENT_METHODS } from "@/lib/constants";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { updateUserPaymentMethod } from "@/lib/actions/user.actions";
 
 const PaymentMethodForm = ({
   preferredPaymentMethod,
@@ -17,7 +28,7 @@ const PaymentMethodForm = ({
   const router = useRouter();
   const { toast } = useToast();
 
-  const form = useForm()<z.infer<typeof paymentMethodSchema>>({
+  const form = useForm<z.infer<typeof paymentMethodSchema>>({
     resolver: zodResolver(paymentMethodSchema),
     defaultValues: {
       type: preferredPaymentMethod || DEFAULT_PAYMENT_METHOD,
@@ -25,11 +36,82 @@ const PaymentMethodForm = ({
   });
 
   const [isPending, startTransition] = useTransition();
+  const onSubmit = async (values: z.infer<typeof paymentMethodSchema>) => {
+    startTransition(async () => {
+      const res = await updateUserPaymentMethod(values);
+      if (!res.success) {
+        toast({
+          variant: "destructive",
+          description: res.message,
+        });
+        return;
+      }
+      router.push("/place-order");
+    });
+  };
 
   return (
-    <div>
-      <CheckoutSteps current={2} />
-    </div>
+    <>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Payment Method</h1>
+        <p className="text-sm text-muted-foreground">
+          Please selct a payment method
+        </p>
+        <Form {...form}>
+          <form
+            method="post"
+            className="space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col gap-5 md:flex-row">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem className="space-y-3">
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        className="flex flex-col space-y-2"
+                      >
+                        {PAYMENT_METHODS.map((paymnetMethod) => (
+                          <FormItem
+                            key={paymnetMethod}
+                            className="flex items-center space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <RadioGroupItem
+                                value={paymnetMethod}
+                                checked={field.value === paymnetMethod}
+                              />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {paymnetMethod}
+                            </FormLabel>
+                          </FormItem>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="animate-spin w-4 h-4" />
+                ) : (
+                  <ArrowRight className="w-4 h-4" />
+                )}
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </>
   );
 };
 
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -32,7 +32,7 @@ export const shippingAddressDefaultValue = {
 
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   ? process.env.PAYMENT_METHODS.split(", ")
-  : ["PayPal, Stripe, CashOnDelivery"];
+  : ["PayPal", "Stripe", "CashOnDelivery"];
 
 export const DEFAULT_PAYMENT_METHOD =
-  process.env.DEFAUL_PAYMENT_METHOD || "PayPal";
+  process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
